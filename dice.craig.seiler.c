975221e26754598dae1017499244d843e7fdfbad
Craig Seiler <cseiler@localhost.localdomain>
2015-10-27 10:07:44 -0400

    Created a template for dice program .c file

---
+++ dice.craig.seiler.c
@@ -0,0 +1,13 @@
+/* Preprocessor  */
+
+/* prototype */
+
+/* Definition of main */
+
+/* Definition of rollDice */
+
+/* Definition of addDice  */
+
+/* Definition of countSum  */
+
+/* Definition of printHistogram */
