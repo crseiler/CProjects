b563ff3e09b0fe6376641526b7c7959c4153f007
craig seiler <cseiler@localhost.localdomain>
2015-10-28 08:10:03 -0400

    Create printhistogram() and execution in main()

--- dice.craig.seiler.c
+++ dice.craig.seiler.c
@@ -8,14 +8,13 @@
 /* prototype */
 void rollDice(int *dice1, int *dice2);
 void addDice(int *sum, int *dice1, int *dice2);
+void printHistogram(int values[]);
 
 /* Definition of main */
 int main()
 {
  int dice1, dice2, sum, increment, test;
- int dp1 = 0, dp2 = 0, dp3 = 0, dp4 = 0,
-    dp5 = 0, dp6 = 0, dp7 = 0, dp8 = 0, 
-    dp9 = 0, dp10 = 0, dp11 = 0, dp12 = 0;
+ int values[13] = {0};
 
 for( increment = 0 ; increment < ROLLTIMES ; increment++ ) 
  {
@@ -25,46 +24,33 @@ for( increment = 0 ; increment < ROLLTIMES ; increment++ )
  printf("%d\n", sum);
 switch (sum) 
      {
-         case 1: dp1++; 
+         case 1: values[1]++; 
          break;
-         case 2: dp2++; 
+         case 2: values[2]++; 
          break;
-         case 3: dp3++; 
+         case 3: values[3]++; 
          break;
-         case 4: dp4++; 
+         case 4: values[4]++; 
          break;
-         case 5: dp5++; 
+         case 5: values[5]++; 
          break;
-         case 6: dp6++; 
+         case 6: values[6]++; 
          break;
-         case 7: dp7++; 
+         case 7: values[7]++; 
          break;
-         case 8: dp8++; 
+         case 8: values[8]++; 
          break;
-         case 9: dp9++; 
+         case 9: values[9]++; 
          break;
-         case 10: dp10++; 
+         case 10: values[10]++; 
          break;
-         case 11: dp11++; 
+         case 11: values[11]++; 
          break;
-         case 12: dp12++; 
+         case 12: values[12]++; 
          break;
       }
       }
-printf("Sum of Dice Roll \n--------------------\n");
-printf("01:\t%d\n", dp1);
-printf("02:\t%d\n", dp2);
-printf("03:\t%d\n", dp3);
-printf("04:\t%d\n", dp4);
-printf("05:\t%d\n", dp5);
-printf("06:\t%d\n", dp6);
-printf("07:\t%d\n", dp7);
-printf("08:\t%d\n", dp8);
-printf("09:\t%d\n", dp9);
-printf("10:\t%d\n", dp10);
-printf("11:\t%d\n", dp11);
-printf("12:\t%d\n", dp12);
-
+printHistogram(values);
 }
 
 /* Definition of rollDice */
@@ -112,16 +98,22 @@ switch (rollDice)
 }
 */
 
-/* Definition of printHistogram 
-char printHistogram()
+/* Definition of printHistogram */
+void printHistogram(int values[])
 {
+int i, x;
 printf("Sum of Dice Roll \n--------------------\n");
-printf("01:%d", dp1);
-printf("02:%d", dp2);
-printf("03:%d", dp3);
-printf("01:%4", dp4);
+for(i=2; i<=12;i++)
+{
+printf("%d:\t%d", i, values[i]);
+for(x=values[i];x>0;x--)
+{
+printf("*");
 }
-*/
+printf("\n");
+}
+
+}
 
 /* Definition of dicePercentage */
 
